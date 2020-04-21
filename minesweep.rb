ea71049f5160768de46e573d60e8bef3489eb218
Gary <gbautista2@knights.ucf.edu>
2020-04-21 15:34:56 -0400

    created #initialize and #game,#win,#get_input,#parse skeletons

---
+++ minesweep.rb
@@ -0,0 +1,22 @@
+class Minesweep
+    def initialize
+        @grid = Board.new
+    end
+end
+
+
+def game
+#run game until you hit a bomb or you clear the board
+end
+
+def win?
+#does the board have any ""? if no then WIN!
+end
+
+def get_input
+#get input from player
+end
+
+def parse(string)
+#change string into coordinate
+end
