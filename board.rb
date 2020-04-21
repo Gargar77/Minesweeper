64e2b3feea58ec8fe20f84731528ea2103b2c538
Gary <gbautista2@knights.ucf.edu>
2020-04-21 14:42:16 -0400

    added #fringe_count and #adjacent_pos helper

--- board.rb
+++ board.rb
@@ -19,7 +19,7 @@ class Board
     def reveal_neighbors(pos)
         row,col = pos
        if no_flag_or_bomb?(pos) && legal_pos?(pos)
-        @rows[row][col] = 'X'
+        @rows[row][col] = fringe_count(pos)
         show_row(pos)
         show_col(pos)
         show_diagnol(pos)
@@ -37,7 +37,7 @@ class Board
         sides.each do |side|
             if no_flag_or_bomb?(side) && legal_pos?(side)
                 s_row,s_col = side
-                @rows[s_row][s_col] = "X"
+                @rows[s_row][s_col] = fringe_count(side)
             end
         end
     end
@@ -51,7 +51,7 @@ class Board
         up_down.each do |c|
             if no_flag_or_bomb?(c) && legal_pos?(c)
                 c_row,c_col = c
-                @rows[c_row][c_col] = "X"
+                @rows[c_row][c_col] = fringe_count(c)
             end
         end
     end
@@ -67,7 +67,7 @@ class Board
         diagnols.each do |diagnol|
             if no_flag_or_bomb?(diagnol) && legal_pos?(diagnol)
                 d_row,d_col = diagnol
-                @rows[d_row][d_col] = "X"
+                @rows[d_row][d_col] = fringe_count(diagnol)
             end
         end
                 
@@ -89,6 +89,35 @@ class Board
             return false
         end
         true
+    end
+
+    def fringe_count(pos)
+        adjacent = adjacent_pos(pos)
+        count = 0
+        adjacent.each do |adj|
+            row,col = adj
+            if @rows[row][col] == "B" && legal_pos?(adj)
+                count += 1
+            end
+        end
+
+        return count
+    end
+
+    def adjacent_pos(pos)
+        row,col = pos
+        up_right = [(row - 1),(col -1)]
+        up_left = [(row -1),(col + 1)]
+        low_right = [(row + 1),(col + 1)]
+        low_left = [(row + 1),(col - 1)]
+        up = [(row - 1),col]
+        down = [(row + 1),col]
+        left_row = [row,(col - 1)] 
+        right_row = [row,(col + 1)]
+
+        adjacent = [up_right,up_left,low_right,low_left,up,down,left_row,right_row]
+
+        return adjacent
     end
 
    
